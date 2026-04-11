59f878927fdce43ab7b9d6ac5fce41e7bb122899
Phuc Nguyen Hoang <nhplhp2004@gmail.com>
2026-04-11 14:39:48 +0700

    feat: init unit tests

---
+++ UnitTests/EntityTests.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Slo248.
+// Licensed under the MIT License.
+
+namespace UnitTests;
+
+public class EntityTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(1 + 1, 2);
+    }
+}
