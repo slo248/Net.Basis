439d1ce990e3f7527e8af52587f49884925ac5b5
Phuc Nguyen Hoang <nhplhp2004@gmail.com>
2026-04-11 15:15:43 +0700

    feat: add domain services

---
+++ Slo248.Net.DomainDrivenDesign/Services/IDomainEventPublisher.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Slo248.
+// Licensed under the MIT License.
+
+using Slo248.Net.DomainDrivenDesign.Models;
+
+namespace Slo248.Net.DomainDrivenDesign.Services;
+
+public interface IDomainEventPublisher
+{
+    Task Publish(IDomainEvent[] events, CancellationToken cancellationToken = default);
+}
---
+++ Slo248.Net.DomainDrivenDesign/Services/IRepository.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Slo248.
+// Licensed under the MIT License.
+
+using Slo248.Net.DomainDrivenDesign.Models;
+
+namespace Slo248.Net.DomainDrivenDesign.Services;
+
+public interface IRepository<TAggregateRoot, in TId> where TAggregateRoot : IAggregateRoot
+{
+    Task<TAggregateRoot?> Get(TId id, CancellationToken cancellationToken = default);
+    Task Add(TAggregateRoot aggregate, CancellationToken cancellationToken = default);
+    Task Update(TAggregateRoot aggregateRoot, CancellationToken cancellationToken = default);
+    Task Delete(TAggregateRoot aggregateRoot, CancellationToken cancellationToken = default);
+}
